4c5eff7c55d21d176702715f2b6741ed5c183a1a
Talar1997 <32823145+Talar1997@users.noreply.github.com>
2021-04-18 17:34:25 +0200

    update

--- figure/figure.R
+++ figure/figure.R
@@ -5,10 +5,10 @@
 
 figure <- function(runs) {
   xs <- runif(runs, min = 0, max = 8)
-  ys <- runif(runs, min = -0, max = 2)
+  ys <- runif(runs, min = 0, max = 2)
 
   in.r_triangle <- xs <= 2 & ys <= 2 & ys <= xs
-  in.mid_square <- xs <= 6 & xs >= 2 & ys <= 1
+  in.mid_rect <- xs <= 6 & xs >= 2 & ys <= 1
   in.l_triangle <- xs >= 6 &
     xs <= 8 &
     ys <= 1 &
@@ -18,17 +18,17 @@ figure <- function(runs) {
     ys >= 1 &
     ys <= (-0.25 * xs + 2.5)
 
-  in_all_figures <- in.r_triangle +
-    in.mid_square +
+  in.all_figures <- in.r_triangle +
+    in.mid_rect +
     in.mid_triangle +
     in.l_triangle
 
-  mc_figure_field <- (sum(in_all_figures) / runs) * 16
   mc_exact_figure_field <- (2 * 2 / 2) + (1 * 4 / 2) + (1 * 2 / 2) + (1 * 4)
+  mc_figure_field <- (sum(in.all_figures) / runs) * 16
 
   mc.error <- abs(mc_exact_figure_field - mc_figure_field)
   plot(xs, ys,
-       col = ifelse(in_all_figures, "blue", "grey"),
+       col = ifelse(in.all_figures, "blue", "grey"),
        xlab = '', ylab = '', asp = 1, pch = '.',
        main = paste("Figure field =", mc_figure_field, ", Result error =", mc.error))
 }
